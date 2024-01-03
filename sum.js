fa878c7ccf5d54ae019aad9e2aca9f594bf755e1
Joyal Demo <91115763+Joyalofficial@users.noreply.github.com>
2024-01-03 22:08:14 +0530

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,5 +1,18 @@
-// Basic
+// program that checks if the number is positive, negative or zero
+// input from the user
+const number = parseInt(prompt("Enter a number: "));
 
-console.log("How are Tou");
-console.log("How are Tou");
-console.log("How are Tou");
+// check if number is greater than 0
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+// check if number is 0
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+// if number is less than 0
+else {
+     console.log("The number is negative");
+}
